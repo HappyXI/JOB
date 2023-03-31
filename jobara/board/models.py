b37e53a02210a4d32845e18b9b0f67dffb147280
정혜원(Jeong Hyeone) <118719561+oort2@users.noreply.github.com>
2023-03-31 22:24:44 +0900

    Update models.py

--- jobara/board/models.py
+++ jobara/board/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+     
+    content= models.CharField(max_length=5000)
+     
+
+def __str__(self):
+    return str(self.num) + ":" + self.subject
