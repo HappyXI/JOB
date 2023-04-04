99034f002fd255a49e18b27a20fc6d1ca3b987ad
JiHyun-Son <wlgusdl5736@naver.com>
2023-04-04 15:11:10 +0900

    add migrations

--- jobara/board/models.py
+++ jobara/board/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Board(models.Model):
+    
+    num = models.AutoField(primary_key=True)
+    id = models.CharField(max_length=20, null=True)
+    subject = models.CharField(max_length=100)
+    content = models.CharField(max_length=5000)
+    regdate = models.DateTimeField(null = True) #null 허용
+    readcnt = models.IntegerField(default = 0)
+    file1 = models.CharField(max_length=300)
      
-    content= models.CharField(max_length=5000)
-     
-
-def __str__(self):
-    return str(self.num) + ":" + self.subject
+    def __str__(self):
+        return str(self.num) + ":" + self.subject
--- jobara/member/models.py
+++ jobara/member/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model) :
+    
+    id = models.CharField(max_length=20, primary_key=True)
+    board = models.IntegerField(null= True)
+    name = models.CharField(max_length=20)
+    pass1 = models.CharField(max_length=20)
+    gender = models.IntegerField(default=0)
+    tel = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    picture = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    birthday = models.DateTimeField(null = True)
+    
+    #def __repr__(self) : 같은 함수
+    def __str__(self):
+        return self.id + ":" + self.name + ":" +self.pass1
